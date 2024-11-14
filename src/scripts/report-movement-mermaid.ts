5c2a2345d08a733ffd15ebb1dd92ed001fdce2a2
Jeff Eaton <jeff@eaton.fyi>
2024-11-13 21:39:55 -0600

    JSON output for vega sankey

--- src/scripts/report-movement-mermaid.ts
+++ src/scripts/report-movement-mermaid.ts
@@ -1,21 +1,31 @@
 import { visualizeMovement } from "../reports/sankey-compare.js";
+import { csv } from "../util/serializers.js";
 import jetpack from "fs-jetpack";
+
+// Ultra jank, but it works.
+const csvHeader = '"source","target","value"';
+const mermaidHeader = `---
+config:
+  sankey:
+    showValues: false
+---
+sankey-beta`;
 
 let dir = jetpack.dir('output/movement/proposed');
 for (const model of ['oai-text-mini', 'google-text', 'mxbai-query', 'minilm', 'smollm2', 'phi35']) {
   // Proximity
   let data = await visualizeMovement({ model, technique: 'proximity', variant: 'proposed' });
-  dir.write('proximity-' + model + '.json', getData(data), { jsonIndent: 0});
+  dir.write('proximity-' + model + '.csv', csv.stringify(data, { quoted_string: true }).replace(csvHeader, mermaidHeader));
 
   // Clustering
   data = await visualizeMovement({ model, technique: 'cluster', variant: 'kmeans' });
-  dir.write('cluster-' + model + '.json', getData(data), { jsonIndent: 0});
+  dir.write('cluster-' + model + '.csv', csv.stringify(data, { quoted_string: true }).replace(csvHeader, mermaidHeader));
 }
 
 // Prompting
 for (const model of ['gemma2', 'llama31-large', 'qwen25-large', 'gpt-4o-mini', 'gemini-small']) {
   const data = await visualizeMovement({ model, technique: 'prompt', variant: 'proposed' });
-  dir.write('prompt-' + model + '.json', getData(data), { jsonIndent: 0});
+  dir.write('prompt-' + model + '.csv', csv.stringify(data, { quoted_string: true }).replace(csvHeader, mermaidHeader));
 }
 
 
@@ -23,32 +33,13 @@ dir = jetpack.dir('output/movement/existing');
 for (const model of ['oai-text-mini', 'google-text', 'mxbai-query', 'minilm']) {
   // Proximity
   let data = await visualizeMovement({ model, technique: 'proximity', variant: 'existing' });
-  dir.write('proximity-' + model + '.json', getData(data), { jsonIndent: 0});
+  dir.write('proximity-' + model + '.csv', csv.stringify(data, { quoted_string: true }).replace(csvHeader, mermaidHeader));
 }
 
 // Prompting
 for (const model of ['gemma2', 'llama31-large', 'qwen25-large', 'gpt-4o-mini', 'gemini-small']) {
   const data = await visualizeMovement({ model, technique: 'prompt', variant: 'existing' });
-  dir.write('prompt-' + model + '.json', getData(data), { jsonIndent: 0});
+  dir.write('prompt-' + model + '.csv', csv.stringify(data, { quoted_string: true }).replace(csvHeader, mermaidHeader));
 }
 
-function getData(input: { source: string, target: string, value: number }[] = []) {
-  const records = input.map(i => ({
-    source: i.source.replace(':', ' '), // Incredibly cheesy hack to get around duplicate in/out categories
-    target: i.target,
-    value: i.value,
-  }));
-  const output = new Set<string>();
-  for (const i of input) {
-    output.add(i.source);
-    output.add(i.target);
-  };
-  return [
-    ...records,
-    ...output.values().map(s => ({ 
-      category: s,
-      stack: 0,
-      gap: 0,
-    }))
-  ];
-}
+
